da1deb5fba31d97d2e09ea3435809e905b35a0ef
Lloyd Cundiff <lloyd@artsoftllc.com>
2015-06-13 10:52:48 -0600

    hello nerds

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,11 @@
 var gulp = require('gulp');
 var awspublish = require("gulp-awspublish");
+var fs = require('fs');
 
 gulp.task('default', function() {
-    var publisher = awspublish.create({
-        accessKeyId: "",
-        secretAccessKey: "",
-        region: "",
-        params: {
-            Bucket: ''
-        }
-    });
+    var publisher = awspublish.create(JSON.parse(fs.readFileSync('.aws.json', 'utf8')));
 
-    return gulp.src('./src/**/*')
+    return gulp.src('./app/**/*')
         .pipe(publisher.publish())
         .pipe(publisher.sync())
         .pipe(awspublish.reporter());
